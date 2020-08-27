4f6d431116f85502a68e70a49759cbce36107196
Yaguang Li <yaguang.li@mail.bnu.edu.cn>
2020-08-27 17:51:03 +0800

    Small bugs fixed.

--- SolarlikePeakbagging.py
+++ SolarlikePeakbagging.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib
-matplotlib.use("Agg")
+# matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import os
 
@@ -11,6 +11,95 @@ from scipy.signal import find_peaks
 
 from Bayesian import FitParameters, Priors, Likelihoods, Posteriors
 from Fitter import FitModes, PTSampler, ESSampler, LSSampler
+
+def echelle(x, y, period, fmin=None, fmax=None, echelletype="single", offset=0.0):
+	'''
+	Generate a z-map for echelle plotting.
+
+	Input:
+
+	x: array-like[N,]
+	y: array-like[N,]
+	period: the large separation,
+	fmin: the lower boundary
+	fmax: the upper boundary
+	echelletype: single/replicated
+	offset: the horizontal shift
+
+	Output:
+
+	x, y: 
+		two 1-d arrays.
+	z: 
+		a 2-d array.
+
+	Exemplary call:
+
+	import matplotlib.pyplot as plt
+	fig = plt.figure(figsize=(6,8))
+	ax1 = fig.add_subplot(111)
+	echx, echy, echz = echelle(tfreq,tpowers_o,dnu,numax-9.0*dnu,numax+9.0*dnu,echelletype="single",offset=offset)
+	levels = np.linspace(np.min(echz),np.max(echz),500)
+	ax1.contourf(echx,echy,echz,cmap="gray_r",levels=levels)
+	ax1.axis([np.min(echx),np.max(echx),np.min(echy),np.max(echy)])
+	if offset > 0.0:
+		ax1.set_xlabel("(Frequency - "+str("{0:.2f}").format(offset)+ ") mod "+str("{0:.2f}").format(dnu) + " ($\mu$Hz)")
+	if offset < 0.0:
+		ax1.set_xlabel("(Frequency + "+str("{0:.2f}").format(np.abs(offset))+ ") mod "+str("{0:.2f}").format(dnu) + " ($\mu$Hz)")
+	if offset == 0.0:
+		ax1.set_xlabel("Frequency mod "+str("{0:.2f}").format(dnu) + " ($\mu$Hz)")
+	plt.savefig("echelle.png")
+
+	'''
+
+	if not echelletype in ["single", "replicated"]:
+		raise ValueError("echelletype is on of 'single', 'replicated'.")
+
+	if len(x) != len(y): 
+		raise ValueError("x and y must have equal size.")	
+
+	if fmin is None: fmin=0.
+	if fmax is None: fmax=np.nanmax(x)
+
+	fmin = fmin - offset
+	fmax = fmax - offset
+	x = x - offset
+
+	if fmin <= 0.0:
+		fmin = 0.0
+	else:
+		fmin = fmin - (fmin % period)
+
+	# first interpolate
+	samplinginterval = np.median(x[1:-1] - x[0:-2]) * 0.1
+	xp = np.arange(fmin,fmax+period,samplinginterval)
+	yp = np.interp(xp, x, y)
+
+	n_stack = int((fmax-fmin)/period)
+	n_element = int(period/samplinginterval)
+	#print(n_stack,n_element,len())
+
+	morerow = 2
+	arr = np.arange(1,n_stack) * period # + period/2.0
+	arr2 = np.array([arr,arr])
+	yn = np.reshape(arr2,len(arr)*2,order="F")
+	yn = np.insert(yn,0,0.0)
+	yn = np.append(yn,n_stack*period) + fmin #+ offset
+
+	if echelletype == "single":
+		xn = np.arange(1,n_element+1)/n_element * period
+		z = np.zeros([n_stack*morerow,n_element])
+		for i in range(n_stack):
+			for j in range(i*morerow,(i+1)*morerow):
+				z[j,:] = yp[n_element*(i):n_element*(i+1)]
+	if echelletype == "replicated":
+		xn = np.arange(1,2*n_element+1)/n_element * period
+		z = np.zeros([n_stack*morerow,2*n_element])
+		for i in range(n_stack):
+			for j in range(i*morerow,(i+1)*morerow):
+				z[j,:] = np.concatenate([yp[n_element*(i):n_element*(i+1)],yp[n_element*(i+1):n_element*(i+2)]])
+
+	return xn, yn, z
 
 class SolarlikePeakbagging:
 	"""docstring for SolarlikePeakbagging"""
@@ -361,6 +450,7 @@ class SolarlikePeakbagging:
 
 		# save a table
 		# but first let's associate each mode with a group number
+		mode_freq, mode_l = np.concatenate(mode_freq), np.concatenate(mode_l)
 		mode_freq_group, mode_l_group, mode_group = [], [], np.array([])
 		idx = np.argsort(mode_freq)
 		mode_freq, mode_l = mode_freq[idx], mode_l[idx]
@@ -529,7 +619,7 @@ class SolarlikePeakbagging:
 		### visulization (left) - plot echelle and collapsed echelle to locate peak
 		# ax1 = plt.subplot2grid((5,3), (0,0), rowspan=4)
 		ax1 = fig.add_subplot(1,2,1)
-		echx, echy, echz = se.echelle(freq, powers, dnu, freq.min(), freq.max(), 
+		echx, echy, echz = echelle(freq, powers, dnu, freq.min(), freq.max(), 
 			echelletype="single", offset=offset)
 		levels = np.linspace(np.min(echz), np.max(echz), 500)
 		ax1.contourf(echx, echy, echz, cmap="gray_r", levels=levels)
@@ -688,7 +778,7 @@ class SolarlikePeakbagging:
 		cdata.fnyq = fnyq
 
 		# specify output directory
-		filepath = self._outputdir+self._sep+"pkbg"+self._sep
+		filepath = self._outputdir+"pkbg"+self._sep
 		if not os.path.exists(filepath): os.mkdir(filepath)
 		filepath = filepath+str(igroup)+self._sep
 		if not os.path.exists(filepath): os.mkdir(filepath)
